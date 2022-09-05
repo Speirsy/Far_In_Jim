513ba043435cf9671fe0c046e9fe4876fded2556
Speirsy <jpspeirs@icloud.com>
2022-09-05 12:10:24 +0100

    apply CSS to FarInJIm.js

--- src/components/FarInJim.js
+++ src/components/FarInJim.js
@@ -1,10 +1,10 @@
 import React from 'react';
-// import { StyleSheet, Text, TouchableOpacity, View} from 'react-native';
-// 
+import './myStyles.css' 
+
 const FarInJim = () => (
   <div>
-    <h4>Far In Jim</h4>
-    <p>Far In Jim Makes Money</p>
+    <h4 className='primary'> Far In Jim</h4>
+    <p className='primary'>Far In Jim Makes Money</p>
   </div>
 
   );

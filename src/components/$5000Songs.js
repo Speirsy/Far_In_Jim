2ad7d9b32aac6fccdfa0862869d5bae46dabc0fd
Speirsy <jpspeirs@icloud.com>
2022-10-03 14:39:53 +0100

    some css

--- src/components/$5000Songs.js
+++ src/components/$5000Songs.js
@@ -1,8 +1,18 @@
 import React from 'react'
+import './myStyles.css' 
 
 function $5000Songs() {
   return (
-    <div>$5000Songs</div>
+    <body className='body'>
+    <div>
+    
+    <h1 className='centre'> $5000 Songs</h1>
+    <h3>Unique Music Ownership experience</h3>
+    <button>Funky Song</button>
+    <button>Bad Salad</button>
+    </div>
+    </body>
+
   )
 }
 
--- src/components/Videos.js
+++ src/components/Videos.js
@@ -3,6 +3,7 @@ import React from 'react'
 function Videos() {
   return (
     <div>Videos</div>
+    // <p> get farinjim videos in here </p>
   )
 }
 

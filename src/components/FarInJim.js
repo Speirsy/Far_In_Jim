e1abf743c6eb2afcbf60cba42ae7eb9c83fbb812
Speirsy <jpspeirs@icloud.com>
2022-12-19 14:22:15 +0000

    service

--- src/components/FarInJim.js
+++ src/components/FarInJim.js
@@ -4,7 +4,7 @@ import './myStyles.css'
 const FarInJim = () => (
   <div>
     {/* <h4 className={className}> Far In Jim</h4>  */}
-    <p className='primary'>Far In Jim Makes Money</p>
+    <p className='primary'>Far In Jim Makes Some Money</p>
   </div>
 
   );

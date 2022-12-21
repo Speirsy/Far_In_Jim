a8c4ce749adf9b4c41a732d4c480adc0de4f338d
Speirsy <jpspeirs@icloud.com>
2022-12-21 12:12:47 +0000

    added text to  songs

--- src/components/$5000Songs.js
+++ src/components/$5000Songs.js
@@ -7,7 +7,40 @@ function $5000Songs() {
     <div>
     
     <h1 className='centre'> $5000 Songs</h1>
-    <h3>Unique Music Ownership experience</h3>
+    <h3>Unique Music Ownership experience
+      <ul>
+        <li>
+        Fully unique music ownership opportunity.
+
+        One great recorded song encapsulated in one playback device. 
+
+        </li>
+        <li>
+        The recorded song will not be distributed via any other service or platform whatsoever.
+        </li>
+      </ul>
+
+
+
+
+
+
+
+
+The recorded song will not be distributed via any other service whatsoever.
+
+
+
+The recorded song will never be made available to broadcasters. 
+
+
+
+The recorded song will be purchased without preview unless auditioned in person 
+
+
+
+Be the sole owner of the physical musical artefact (farinjim retains copyright) and the NFT
+    </h3>
     <button>Funky Song</button>
     <button>Bad Salad</button>
     </div>
--- src/components/Home.js
+++ src/components/Home.js
@@ -6,6 +6,7 @@ const Home = () => (
   <div>
     <h4>Home</h4>
     <p>Welcome to our magical homepage</p>
+    
     <li>    
       {/* <Link to="/$5000songs">$5000Songs</Link>   
       <Link to="/videos">Videos</Link> */}
